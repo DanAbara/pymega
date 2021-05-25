d3c6d546a27bf01a10d1463bfff5d75d791b0687
Daniel Abara <44237659+deehat@users.noreply.github.com>
2021-05-26 00:36:04 +0100

    Capture complete

--- compVision/FaceDetector/face_detector.py
+++ compVision/FaceDetector/face_detector.py
@@ -10,6 +10,9 @@ face_coordinate = face_cascade.detectMultiScale(img_gray,
     scaleFactor=1.1,
     minNeighbors=4) # scaleFactor - no of passes on the image. 1.05 means 5% reduction in size on each pass. Higher values mean less accuracy
 
+print(type(face_coordinate))
+print(face_coordinate)
+
 # draw a rectangle representing the coordinates on the image, x cord, y cord, w width, and h height
 for x,y,w,h in face_coordinate:
     img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3) # imageToUpdate, topLeftCoord, buttomRightCoord, BGRspecifier, rectangleThickness
@@ -18,8 +21,7 @@ for x,y,w,h in face_coordinate:
 
 img=cv2.resize(img,(int(img.shape[1]/3), int(img.shape[0]/3))) # imageToResize, width, height
 
-print(type(face_coordinate))
-print(face_coordinate)
+
 print(img.shape)
 
 cv2.imshow("Gray",img)
--- compVision/VideoCapture/capture.py
+++ compVision/VideoCapture/capture.py
@@ -1,6 +1,35 @@
-import cv2
+import cv2, time
 
-video=cv2.VideoCapture(0)
+# start webcam, default is 0
+video=cv2.VideoCapture(0,cv2.CAP_DSHOW) 
 
+# trick to compute no of frames
+nf = 0
+
+while True:
+    # obtain frames for video capture object, 'check' is bool to confirm video is working
+    # frame is the first frame of the video, python cv2 reads videos as frames of separate images
+    nf = nf + 1
+    check, frame = video.read() 
+
+    print(check)
+    print(frame)
+
+    # Convert to gray scale - 
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    #time.sleep(3) # hold camera on for 3 seconds
+
+    # Show a window
+    cv2.imshow("Capturing", gray)
+
+    key = cv2.waitKey(1) # show next frame after the specified no of seconds
+    
+    # Break loop when key q is pressed
+    if key==ord('q'): 
+        break
+
+print(nf) # show no of frames (no of iterations) after loop breaks
 
 video.release()
+cv2.destroyAllWindows
